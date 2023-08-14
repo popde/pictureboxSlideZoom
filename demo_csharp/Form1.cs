40186e428a36f373d3b43ab859198d05d68abc57
popdes <popdes@126.com>
2023-08-14 09:39:49 +0800

    Add files via upload

---
+++ demo_csharp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userControl11.imgPath = "C:\\Users\\huave2\\Desktop\\捕获.PNG";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            userControl11.imgPath = "C:\\Users\\huave2\\Desktop\\单波长直对准.PNG";
+        }
+    }
+}
